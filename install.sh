c62eb05aeedc49620cb90b830273942cb671e108
Joshua Lester <josh@joshualester.com>
2026-05-17 10:24:49 -0700

    stuff

--- install.sh
+++ install.sh
@@ -11,14 +11,14 @@ systemctl enable --now NetworkManager.service
 nmcli d wifi connect NETGEAR31 --ask
 
 # install pacman stuff (maybe change to pull my main overall list)
-pacman -S sudo plasma sddm dolphin konsole firefox plasma-nm kscreen powerdevil plasma-pa
+pacman -S # requirements.txt
 
 # security stuff
 echo "joshu freyja=(ALL:ALL) ALL" >> /etc/sudoers.d/90-joshu
 passwd -l root
 echo "Defaults pwfeedback" >> /etc/sudoers.d/10-pwfeedback
 
-systemctl enable sddm
+systemctl enable plasmalogin.service
 
 # reboot
 
